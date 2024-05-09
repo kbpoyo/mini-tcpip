b740bdc69a3aa689a905ec73c10dcfc38215bec7
kbpoyo <kbpoyo@qq.com>
2024-05-09 10:46:17 +0800

    创建工程

---
+++ build/CMakeFiles/net.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/net.dir/src/net/test.c.obj"
+  "CMakeFiles/net.dir/src/net/test.c.obj.d"
+  "libnet.dll.a"
+  "net.exe"
+  "net.exe.manifest"
+  "net.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
